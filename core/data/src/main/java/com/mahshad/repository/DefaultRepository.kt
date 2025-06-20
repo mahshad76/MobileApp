297e515ca9c9be78e8056c1bf7f4a5d15858d092
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-06-20 15:11:00 -0400

    the getObjects of the repository is implemented and the model and the network modules are added as libraries

--- core/data/src/main/java/com/mahshad/repository/DefaultRepository.kt
+++ core/data/src/main/java/com/mahshad/repository/DefaultRepository.kt
@@ -1,13 +1,31 @@
 package com.mahshad.repository
 
-//import com.mahshad.model.data.Object
-//import javax.inject.Inject
-//
-/////injection of the network module
-//class DefaultRepository @Inject constructor(
-//    //retrofitNetwork: RetrofitNetwork
-//) : Repository {
-//    override fun getObjects(): List<com.mahshad.model.data.Object> {
-//        TODO("Not yet implemented")
-//    }
-//}
+import android.util.Log
+import com.mahshad.model.data.Object
+import com.mahshad.network.ApiService
+import com.mahshad.network.model.toObject
+import javax.inject.Inject
+
+class DefaultRepository @Inject constructor(private val apiService: ApiService) : Repository {
+    override suspend fun getObjects(): List<Object>? {
+        try {
+            val response = apiService.getObjects()
+            if (response.isSuccessful) {
+                return response.body()?.let {
+                    it.map { objectDto ->
+                        objectDto.toObject()
+                    }
+                } ?: emptyList()
+
+            } else {
+                Log.d("TAG", "getObjects is not successful")
+                return null
+
+            }
+        } catch (e: Exception) {
+            Log.e("TAG", "getObjects: an error has occurred, ${e.message}")
+            return null
+        }
+    }
+
+}
--- core/data/src/main/java/com/mahshad/repository/Repository.kt
+++ core/data/src/main/java/com/mahshad/repository/Repository.kt
@@ -1,7 +1,8 @@
 package com.mahshad.repository
 
-//import com.mahshad.model.data.Object
-//
-//interface Repository {
-//    fun getObjects(): List<com.mahshad.model.data.Object>
-//}
+import com.mahshad.model.data.Object
+
+interface Repository {
+    suspend fun getObjects(): List<Object>?
+}
+
