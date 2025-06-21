5dc3048007b25bd0f0c077f4a789c4066762ed41
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-06-21 13:59:58 -0400

    the queries in DAO are defined with using both flow and liveData

--- core/database/src/main/java/com/mahshad/database/DAO.kt
+++ core/database/src/main/java/com/mahshad/database/DAO.kt
@@ -1,12 +1,12 @@
 package com.mahshad.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.mahshad.model.data.Object
 import kotlinx.coroutines.flow.Flow
 
-///DAO methods to be suspend functions or return Flow types
 interface DAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(mobileObject: Object)
@@ -16,6 +16,12 @@ interface DAO {
 
     @Query("select * from Objects")
     fun getAll(): Flow<List<Object>>
+
+    @Query("select * from Objects")
+    fun getAllLively(): LiveData<List<Object>>
+
+    @Query("select * from Objects where object_id=:mobileId")
+    suspend fun searchById(mobileId: Int): Flow<List<Object>>
 
     @Query("DELETE FROM Objects WHERE object_id = :mobileId")
     suspend fun delete(mobileId: Int)
