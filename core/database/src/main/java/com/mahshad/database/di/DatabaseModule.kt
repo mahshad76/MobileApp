0b7ef2ebf005ebd1d7630f53e6f897ad9aaca8cd
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-06-22 13:35:57 -0400

    database is provided with hilt

--- core/database/src/main/java/com/mahshad/database/di/DatabaseModule.kt
+++ core/database/src/main/java/com/mahshad/database/di/DatabaseModule.kt
@@ -1,4 +1,26 @@
 package com.mahshad.database.di
 
+import android.content.Context
+import androidx.room.Room
+import com.mahshad.database.Database
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+
 object DatabaseModule {
+    @Provides
+    @Singleton
+    fun providesObjectDatabase(
+        @ApplicationContext context: Context,
+    ): Database = Room.databaseBuilder(
+        context,
+        Database::class.java,
+        Database.DATABASE_NAME,
+    ).build()
 }
