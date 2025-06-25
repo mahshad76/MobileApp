229185b6d7533d4b64d108fd44801a74aa60c397
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-06-25 15:18:48 -0400

    datastore is created but it is not delegate

--- core/datastore/src/main/java/com/mahshad/datastore/preferencedatastore/Preferences.kt
+++ core/datastore/src/main/java/com/mahshad/datastore/preferencedatastore/Preferences.kt
@@ -1,5 +1,12 @@
 package com.mahshad.datastore.preferencedatastore
 
-class Preferences() {
+import android.content.Context
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class Preferences @Inject constructor(@ApplicationContext context: Context) {
+    val dataStore = preferencesDataStore(name = "app_settings")
+
 
 }
