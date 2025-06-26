2692403df1de5de12eaba42897f126ec4bc10f55
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-06-26 17:20:05 -0400

    write to the preference datastore

--- core/datastore/src/main/java/com/mahshad/datastore/preferencedatastore/Preferences.kt
+++ core/datastore/src/main/java/com/mahshad/datastore/preferencedatastore/Preferences.kt
@@ -1,12 +1,32 @@
 package com.mahshad.datastore.preferencedatastore
 
 import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
-class Preferences @Inject constructor(@ApplicationContext context: Context) {
-    val dataStore = preferencesDataStore(name = "app_settings")
+class Preferences @Inject constructor(@ApplicationContext private val context: Context) {
+    companion object {
+        private const val USER_PREFERENCES_NAME = "user_preferences"
+    }
 
+    private object PreferencesKeys {
+        val SHOW_COMPLETED = stringPreferencesKey("show_completed")
+    }
 
+    private val Context.dataStore by preferencesDataStore(
+        name = Companion.USER_PREFERENCES_NAME
+    )
+
+    suspend fun writeToDataStore(value: String) {
+        context.dataStore.edit { preferences ->
+            preferences[PreferencesKeys.SHOW_COMPLETED] = value
+
+        }
+    }
+
+    suspend fun readFromDataStore() {
+    }
 }
