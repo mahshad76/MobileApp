a7ff2ffc3b1812787ba3696733a384b90d7cf7bc
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-06-28 16:37:25 -0400

    di for the preference datastore

---
+++ core/datastore/src/main/java/com/mahshad/datastore/di/DataStoreModule.kt
@@ -0,0 +1,20 @@
+package com.mahshad.datastore.di
+
+import android.content.Context
+import com.mahshad.datastore.preferencedatastore.PreferenceDataStoreManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+    @Singleton
+    @Provides
+    fun providePreferenceDataStore(@ApplicationContext context: Context): PreferenceDataStoreManager =
+        PreferenceDataStoreManager(context)
+
+}
--- core/datastore/src/main/java/com/mahshad/datastore/preferencedatastore/PreferenceDataStoreManager.kt
+++ core/datastore/src/main/java/com/mahshad/datastore/preferencedatastore/PreferenceDataStoreManager.kt
@@ -6,17 +6,15 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import javax.inject.Inject
 
 object PreferencesKeys {
     val USER_NAME = stringPreferencesKey("user_name")
 }
 
-class PreferenceDataStoreManager @Inject constructor
-    (@ApplicationContext private val context: Context) {
+class PreferenceDataStoreManager
+    (private val context: Context) {
     private val Context.dataStore: DataStore<Preferences> by
     preferencesDataStore(name = "user_preferences")
 
