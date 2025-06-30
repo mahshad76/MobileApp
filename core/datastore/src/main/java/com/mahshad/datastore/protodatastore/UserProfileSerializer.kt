bd410dbc92c511bc642fcad879e85b668b637c0a
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-06-30 19:14:26 -0400

    the proto data store serializer is created

--- core/datastore/src/main/java/com/mahshad/datastore/protodatastore/UserProfileSerializer.kt
+++ core/datastore/src/main/java/com/mahshad/datastore/protodatastore/UserProfileSerializer.kt
@@ -5,9 +5,9 @@ import com.mahshad.model.proto.UserProfile
 import java.io.InputStream
 import java.io.OutputStream
 
+
 object UserProfileSerializer : Serializer<UserProfile> {
-    override val defaultValue: UserProfile
-        get() = TODO("Not yet implemented")
+    override val defaultValue: UserProfile = UserProfile.getDefaultInstance()
 
     override suspend fun readFrom(input: InputStream): UserProfile {
         TODO("Not yet implemented")
