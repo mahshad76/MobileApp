66626420c7c17fa9bc59a833a47947767e83ca70
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-07-02 19:18:19 -0400

    deleting the manager file for the preference repository

--- core/datastore/src/main/java/com/mahshad/datastore/di/DataStoreModule.kt
+++ core/datastore/src/main/java/com/mahshad/datastore/di/DataStoreModule.kt
@@ -1,26 +1,18 @@
 package com.mahshad.datastore.di
 
-import android.content.Context
 import com.mahshad.datastore.preferencedatastore.AppPreferenceRepositoryImpl
 import com.mahshad.datastore.preferencedatastore.AppPreferencesRepository
-import com.mahshad.datastore.preferencedatastore.PreferenceDataStoreManager
 import com.mahshad.datastore.protodatastore.UserProfileRepository
 import com.mahshad.datastore.protodatastore.UserProfileRepositoryImpl
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DataStoreModule {
-    @Singleton
-    @Provides
-    fun providePreferenceDataStore(@ApplicationContext context: Context): PreferenceDataStoreManager =
-        PreferenceDataStoreManager(context)
 
     @Singleton
     @Binds
