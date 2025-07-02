01c2a8646a34ff3d8b91abbdf872c3ae82d8ada7
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-07-02 13:23:26 -0400

    the edit functions are implemented for the preference datastore

--- core/datastore/src/main/java/com/mahshad/datastore/preferencedatastore/AppPreferencesRepository.kt
+++ core/datastore/src/main/java/com/mahshad/datastore/preferencedatastore/AppPreferencesRepository.kt
@@ -1,6 +1,6 @@
 package com.mahshad.datastore.preferencedatastore
 
 interface AppPreferencesRepository {
-    suspend fun saveUserName()
-    suspend fun savePassword()
+    suspend fun saveUserName(username: String)
+    suspend fun savePassword(password: String)
 }
