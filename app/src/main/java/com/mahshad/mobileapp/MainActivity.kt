15eec682d9a80e15349b8da33e1689ee7f2a2800
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-08-06 17:01:15 -0400

    the application graph is given as my nav host to the main activity layout, and in the app graph it has a fragment which refers to the main fragment and it is set as the first destination in my app navigation graph

--- app/src/main/java/com/mahshad/mobileapp/MainActivity.kt
+++ app/src/main/java/com/mahshad/mobileapp/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mahshad.mobileapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(
             findViewById
                 (R.id.app_fragment_container)
@@ -23,5 +26,6 @@ class MainActivity : AppCompatActivity() {
             )
             insets
         }
+        //NavHostController()
     }
 }
