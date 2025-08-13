8c7433cfdb6d06b5c5218c167e8b14857ddaee65
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-08-13 12:26:24 -0400

    providing the datastores delegately as an extension property on the Context

--- core/datastore/src/main/java/com/mahshad/datastore/di/DataStoreModule.kt
+++ core/datastore/src/main/java/com/mahshad/datastore/di/DataStoreModule.kt
@@ -2,11 +2,11 @@ package com.mahshad.datastore.di
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.dataStore
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.emptyPreferences
-import androidx.datastore.preferences.preferencesDataStoreFile
+import androidx.datastore.preferences.preferencesDataStore
+import com.mahshad.core.datastore.UserProfile
+import com.mahshad.datastore.UserProfileSerializer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,6 +16,15 @@ import javax.inject.Singleton
 
 val USER_PREFERENCES = "user_preferences"
 
+val Context.userProfileDataStore: DataStore<UserProfile> by dataStore(
+    fileName = "my_data.pb",
+    serializer = UserProfileSerializer
+)
+
+val Context.userPreferencesDataStore: DataStore<Preferences> by preferencesDataStore(
+    name = "user_preferences"
+)
+
 @InstallIn(SingletonComponent::class)
 @Module
 object DataStoreModule {
@@ -23,12 +32,11 @@ object DataStoreModule {
     @Provides
     fun providePreferencesDataStore(
         @ApplicationContext appContext: Context
-    ): DataStore<Preferences> {
-        return PreferenceDataStoreFactory.create(
-            corruptionHandler =
-                ReplaceFileCorruptionHandler(produceNewData = { emptyPreferences() }),
-            produceFile =
-                { appContext.preferencesDataStoreFile(USER_PREFERENCES) }
-        )
-    }
+    ): DataStore<Preferences> = appContext.userPreferencesDataStore
+
+    @Singleton
+    @Provides
+    fun provideProtoDataStore(
+        @ApplicationContext appContext: Context
+    ): DataStore<UserProfile> = appContext.userProfileDataStore
 }
