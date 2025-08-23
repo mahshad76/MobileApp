4ceb04024d8d8cd329c42ba9c55a79e498f83cb2
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-08-23 18:15:01 -0400

    the library related to the view model coroutine scope is added

--- feature/home/src/main/java/com/mahshad/home/HomeListViewModel.kt
+++ feature/home/src/main/java/com/mahshad/home/HomeListViewModel.kt
@@ -1,10 +1,23 @@
 package com.mahshad.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.mahshad.model.data.Object
 import com.mahshad.repository.ObjectRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeListViewModel @Inject constructor(objectRepository: ObjectRepository) {
+class HomeListViewModel @Inject constructor(private val objectRepository: ObjectRepository) {
+    ///defining states as state flow or live data, call the repository functions, and set the value
+    ///of the states
+    private val _objectsState: MutableLiveData<Result<List<Object>>?> =
+        MutableLiveData(null)
+    val objectState: LiveData<Result<List<Object>>?> =
+        _objectsState
 
+    fun updateObjectsList() {
+
+        objectRepository.getObjects()
+    }
 }
