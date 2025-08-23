ef5eb742529a501c196e5d8a88a9984f4c9b5e1a
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-08-23 18:27:22 -0400

    checking the type of the called function from the repository in the viewmodel to call the right ui update function

--- feature/home/src/main/java/com/mahshad/home/HomeListViewModel.kt
+++ feature/home/src/main/java/com/mahshad/home/HomeListViewModel.kt
@@ -2,22 +2,32 @@ package com.mahshad.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mahshad.model.data.Object
 import com.mahshad.repository.ObjectRepository
+import com.mahshad.repository.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeListViewModel @Inject constructor(private val objectRepository: ObjectRepository) {
+class HomeListViewModel @Inject constructor(private val objectRepository: ObjectRepository) :
+    ViewModel() {
     ///defining states as state flow or live data, call the repository functions, and set the value
     ///of the states
     private val _objectsState: MutableLiveData<Result<List<Object>>?> =
         MutableLiveData(null)
-    val objectState: LiveData<Result<List<Object>>?> =
-        _objectsState
+    val objectState: LiveData<Result<List<Object>>?> = _objectsState
 
     fun updateObjectsList() {
-
-        objectRepository.getObjects()
+        viewModelScope.launch {
+            val result = objectRepository.getObjects()
+            when (result) {
+                is Result.Error -> TODO()
+                is Result.Loading -> TODO()
+                is Result.Successful<*> -> TODO()
+            }
+        }
     }
 }
