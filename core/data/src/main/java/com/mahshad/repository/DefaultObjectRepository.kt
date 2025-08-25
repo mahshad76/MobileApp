2b0d90d25b284b47d231aec0dfcc04c971810af3
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-08-25 14:49:26 -0400

    in the default object repository the assessment function is added to remove the repetition

--- core/data/src/main/java/com/mahshad/repository/DefaultObjectRepository.kt
+++ core/data/src/main/java/com/mahshad/repository/DefaultObjectRepository.kt
@@ -5,163 +5,72 @@ import com.mahshad.network.ApiService
 import com.mahshad.network.model.ObjectDto
 import com.mahshad.network.model.toObject
 import okhttp3.ResponseBody
+import retrofit2.Response
 import javax.inject.Inject
+
+fun <T, K> assessResponse(response: Response<T?>, operation: (T) -> K): Result<K> =
+    if (response.isSuccessful) {
+        response.body()?.let { body ->
+            try {
+                val transformedData = operation(body)
+                Result.Successful(transformedData)
+            } catch (e: Exception) {
+                Result.Error(e)
+            }
+        } ?: Result.Error(Exception("Response body is null"))
+    } else {
+        val errorBody = response.errorBody()?.string()
+        Result.Error(
+            Exception(
+                "API call failed with code: ${response.code()}, error: $errorBody"
+            )
+        )
+    }
 
 class DefaultObjectRepository @Inject constructor(
     private val apiService: ApiService
 ) : ObjectRepository {
     override suspend fun getObjects(): Result<List<Object>> {
-        return try {
-            val response = apiService.getObjects()
-            if (response.isSuccessful) {
-                val listOfObjects = response.body()?.map { objectDto ->
-                    objectDto.toObject()
-                }
-                if (listOfObjects != null) {
-                    Result.Successful(listOfObjects)
-                } else {
-                    Result.Error(Exception("Response body is null"))
-                }
-            } else {
-                val errorBody = response.errorBody()?.string()
-                Result.Error(
-                    Exception(
-                        "API call failed with code: " +
-                                "${response.code()}, error: $errorBody"
-                    )
-                )
+        val response = apiService.getObjects()
+        return assessResponse(response, { input: List<ObjectDto> ->
+            input.map { objectDto ->
+                objectDto.toObject()
             }
-        } catch (e: Exception) {
-            Result.Error(e)
-        }
+        })
     }
 
     override suspend fun getObjectsById(ids: List<Int>): Result<List<Object>> {
-        return try {
-            val response = apiService.getObjectsById(ids)
-            if (response.isSuccessful) {
-                val listOfObjects = response.body()?.map { objectDto ->
-                    objectDto.toObject()
-                }
-                if (listOfObjects != null) {
-                    Result.Successful(listOfObjects)
-                } else {
-                    Result.Error(Exception("Response body is null"))
-                }
-            } else {
-                val errorBody = response.errorBody()?.string()
-                Result.Error(
-                    Exception(
-                        "API call failed with code: " +
-                                "${response.code()}, error: $errorBody"
-                    )
-                )
+        val response = apiService.getObjectsById(ids)
+        return assessResponse(response, { input: List<ObjectDto> ->
+            input.map { objectDto ->
+                objectDto.toObject()
             }
-        } catch (e: Exception) {
-            Result.Error(e)
-        }
+        })
     }
 
     override suspend fun postAnObject(body: ObjectDto): Result<Object> {
-        return try {
-            val response = apiService.postAnObject(body)
-            if (response.isSuccessful) {
-                val postedObject = response.body()?.toObject()
-                if (postedObject != null) {
-                    Result.Successful(postedObject)
-                } else {
-                    Result.Error(Exception("Response body is null"))
-                }
-            } else {
-                val errorBody = response.errorBody()?.string()
-                Result.Error(
-                    Exception(
-                        "API call failed with code: " +
-                                "${response.code()}, error: $errorBody"
-                    )
-                )
-            }
-        } catch (e: Exception) {
-            Result.Error(e)
-        }
+        val response = apiService.postAnObject(body)
+        return assessResponse(response, { input: ObjectDto -> input.toObject() })
     }
 
     override suspend fun deleteAnObject(id: Int): Result<ResponseBody> {
-        return try {
-            val response = apiService.deleteAnObject(id)
-            if (response.isSuccessful) {
-                val responseBody = response.body()
-                if (responseBody != null) {
-                    Result.Successful(responseBody)
-                } else {
-                    Result.Error(Exception("Response body is null"))
-                }
-            } else {
-                val errorBody = response.errorBody()?.string()
-                Result.Error(
-                    Exception(
-                        "API call failed with code: " +
-                                "${response.code()}, error: $errorBody"
-                    )
-                )
-            }
-        } catch (e: Exception) {
-            Result.Error(e)
-        }
+        val response = apiService.deleteAnObject(id)
+        return assessResponse(response, { it })
     }
 
     override suspend fun partialUpdate(
         id: Int,
         body: Map<String, Any>
     ): Result<Object> {
-        return try {
-            val response = apiService.partialUpdate(id, body)
-            if (response.isSuccessful) {
-                val responseObject = response.body()?.toObject()
-                if (responseObject != null) {
-                    Result.Successful(responseObject)
-                } else {
-                    Result.Error(Exception("Response body is null"))
-                }
-            } else {
-                val errorBody = response.errorBody()?.string()
-                Result.Error(
-                    Exception(
-                        "API call failed with code: " +
-                                "${response.code()}, error: $errorBody"
-                    )
-                )
-            }
-        } catch (e: Exception) {
-            Result.Error(e)
-        }
+        val response = apiService.partialUpdate(id, body)
+        return assessResponse(response, { input: ObjectDto -> input.toObject() })
     }
 
     override suspend fun update(
         id: Int,
         body: ObjectDto
     ): Result<Object> {
-        return try {
-            val response = apiService.update(id, body)
-            if (response.isSuccessful) {
-                val responseObject = response.body()?.toObject()
-                if (responseObject != null) {
-                    Result.Successful(responseObject)
-                } else {
-                    Result.Error(Exception("Response body is null"))
-                }
-            } else {
-                val errorBody = response.errorBody()?.string()
-                Result.Error(
-                    Exception(
-                        "API call failed with code: " +
-                                "${response.code()}, error: $errorBody"
-                    )
-                )
-            }
-        } catch (e: Exception) {
-            Result.Error(e)
-        }
+        val response = apiService.update(id, body)
+        return assessResponse(response, { input: ObjectDto -> input.toObject() })
     }
-
 }
