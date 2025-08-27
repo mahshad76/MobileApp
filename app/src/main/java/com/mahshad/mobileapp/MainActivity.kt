132a09bf06a6b6e7b60beff99f3b976c70b29df0
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-08-27 14:25:43 -0400

    bottom navigator's nav controller is mapped to the nav controller of the fragment container

--- app/src/main/java/com/mahshad/mobileapp/MainActivity.kt
+++ app/src/main/java/com/mahshad/mobileapp/MainActivity.kt
@@ -5,16 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mahshad.mobileapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var bottomNavigator: BottomNavigationView
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        bottomNavigator = mainBinding.bottomNavigator
+        navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.app_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigator.setupWithNavController(navController)
         setContentView(mainBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(
             findViewById
